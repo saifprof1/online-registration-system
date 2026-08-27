fe603df3342284af0ab4f0a658531eb382de25d1
Muhammad Saif <saifmahmud736@gmail.com>
2026-08-27 21:37:17 +0600

    Initial project setup

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Online Registration System</title>
+</head>
+<body>
+
+    <h1>Online Student Registration & Data Management System</h1>
+    <p>Project is working!</p>
+
+</body>
+</html>
